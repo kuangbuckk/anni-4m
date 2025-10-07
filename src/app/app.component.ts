190375489a176fd1453af96abe24e3d8ee73a90a
kuangbuckk <kuangbuckk159@gmail.com>
2025-10-08 01:26:35 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'anniversary-app';
+  isOpened = false;
+
+  toggleCard() {
+    this.isOpened = !this.isOpened;
+  }
+
+  ngOnInit() {
+    const audio = new Audio('/assets/song.mp3');
+    audio.play();
+  }
 }
